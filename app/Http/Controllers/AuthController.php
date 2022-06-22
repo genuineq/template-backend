64c9939341d4735f04b7cc9567bcbb65fe2390d8
StefanGhiban <stefanghiban962@gmail.com>
2022-06-22 17:53:53 +0300

    added redis support

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,6 +32,7 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request): object
     {
+        // \Cache::store('redis')->put('Laradock', 'Awesome', 100);
         $credentials = $request->validated();
 
         if (!Auth::attempt($credentials)) {
